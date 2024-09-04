fd2d60b34dfe4541ad595dfdbaa28958e3981e8e
GrungeElFz <elf0114_@hotmail.com>
2024-09-03 22:58:55 -0500

    Modified:   Removed `X`, `Facebook`, and `RSS` from the footer's `socialLinks` section (src/navigation.js)

--- src/navigation.js
+++ src/navigation.js
@@ -117,10 +117,7 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
