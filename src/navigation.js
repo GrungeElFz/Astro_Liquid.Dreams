bedc298e0dae457685ecc007fbcbf256adb99e3d
GrungeElFz <elf0114_@hotmail.com>
2024-09-03 23:02:37 -0500

    Modified:   Updated `Instagram` and `Github` in the footer's `socialLinks` section (src/navigation.js)

--- src/navigation.js
+++ src/navigation.js
@@ -117,8 +117,8 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/liquiddreams.event' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/GrungeElFz/Astro_Liquid.Dreams' },
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
